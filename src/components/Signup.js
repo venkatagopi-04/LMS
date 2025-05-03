7f829bd32e25418eedc91ab785238705872a63ed
Venkata Gopi Muvva <163830451+venkatagopi-04@users.noreply.github.com>
2025-05-03 11:03:11 +0530

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-const navigate = useNavigate();
+
 
 const Signup = () => {
+    const navigate = useNavigate();
     const [formData, setFormData] = useState({
         username: '',
         role: 'student',
